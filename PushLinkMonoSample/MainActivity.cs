d985e607207c76d8956690c31b47985418b305ba
celiovasconcelos <celiovasconcelos@gmail.com>
2015-04-02 09:54:09 -0300

    First commit

---
+++ PushLinkMonoSample/MainActivity.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Com.Pushlink.Android;
+
+namespace PushLinkMonoSample
+{
+	[Activity (Label = "PushLinkMonoSample", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+		int count = 1;
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+			PushLink.Start(this, Resource.Drawable.Icon, "yourApiKey", "yourDeviceID");
+
+			// Get our button from the layout resource,
+			// and attach an event to it
+			Button button = FindViewById<Button> (Resource.Id.myButton);
+			
+			button.Click += delegate {
+				button.Text = string.Format ("{0} clicks!", count++);
+			};
+		}
+	}
+}
+
+
